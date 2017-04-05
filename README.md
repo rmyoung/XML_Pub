25ae412a4701a2a13cfff63a3da813bf8d07728e
Ji Woong <park.jiwoong@wustl.edu>
2017-04-05 17:59:41 -0500

    Updated with SQL Information

---
+++ README.md
@@ -0,0 +1,17 @@
+# XML to SQL 
+
+##Be sure to git clone this repository into your computer prior to executing the codes.
+
+If necessary, you can comment parts of the codes and uncomment them as you go.
+
+The purpose of this is following:
+
+1. Get familiarized handling XML data. XML is similar to JSON in that it is "semi-structured".
+What that means is that certain level of parsing is required prior to importing for data analysis.
+
+2. Get familiarized with SQL. As you have large amount of data, you do not want to rely on just storing data in memory. This will slow you down (and you will lose data when things crash). SQLite3 is a simple tool that's good to use for learning and applying SQL.
+
+Download SQLite3 here:
+[Download Precompiled Binary According to Your OS](https://sqlite.org/download.html)
+
+3. Get used to handling data in general. Later, you will move to doing analysis in these files as well as plotting figures and whatnot.
--- parsing_XML.py
+++ parsing_XML.py
@@ -17,4 +17,52 @@ print type(root.tag) # this is a string
 print root[0][0] # MedlineCitation
 print root[0][0][0] #PMID
 print root[0][0][1] #DateCreated
+print root[0][0][4][3][0].text # shows the abstract texts
 
+# Iteration
+
+for node in tree.iter():
+	print node.tag, node.attrib # Prints all the nodes tags and attributes
+	# Note that attrib is actually empty... {}
+
+# Using iteration, you can now get all the texts.
+
+for node in tree.iter():
+	if node.tag == 'AbstractText':
+		print node.text
+
+# Save it to the memory
+
+abstractTexts = []
+
+abstractPMID = []
+
+for node in tree.iter():
+	if node.tag == 'AbstractText':
+		abstractTexts.append(node.text)
+	if node.tag == 'PMID':
+		abstractPMID.append(node.text)
+
+print abstractTexts
+print abstractPMID
+
+abstractText_and_PMIDs = zip(abstractPMID,abstractTexts)
+
+print abstractText_and_PMIDs
+
+# Now, save this in SQLite3 file so you can visualize it.
+
+import sqlite3
+conn = sqlite3.connect('Pub.db') # Create a connection to the database
+
+cur = conn.cursor() # Get a cursor on the database. Allows using SQL
+
+cur.execute('DROP TABLE IF EXISTS Pub_Table')
+cur.execute('CREATE TABLE Pub_Table (ID INTEGER PRIMARY KEY AUTOINCREMENT,PMID INTEGER,Abstract TEXT)')
+cur.executemany('INSERT INTO Pub_Table (PMID,Abstract) VALUES(?,?)', abstractText_and_PMIDs) # Putting in data using SQL query
+
+conn.commit() # Commit changes to the database
+
+cur.close() # Close the cursor
+
+conn.close() # Close the database connection
